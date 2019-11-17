3299a82b7c6a4efa6902a19b59a0221fcdf3902e
jeremy <stairs1@github.com>
2019-11-17 18:46:18 -0500

    first iteration of voicerec-based registry

---
+++ abv3.py
@@ -0,0 +1,23 @@
+
+
+class Register():
+    
+    def __init__(self):
+        self.on = False
+        self.registry = []
+
+    def start(self):
+        self.on = True
+        self.record_loop()
+
+    def record_loop(self):
+        while self.on:
+            speech = subprocess.run(['termux-speech-to-text'], capture_output=True)
+            if speech.stdout != b'':
+                self.registry.append(speech)
+            print("current registry: ")
+            for i, reg in enumerate(self.registry):
+                print(i, ".", reg)
+
+reg = Register()
+reg.start()
