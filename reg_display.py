f6db465b2501a555c48a227db5707daaf97ff2d4
jeremy <stairs1@github.com>
2019-11-20 21:52:25 -0500

    static webpage showing audio regisrty

--- reg_display.py
+++ reg_display.py
@@ -1,5 +1,9 @@
 import socket
 import time
+import json
+from flask import Flask, render_template
+app = Flask(__name__)
+import threading
 
 
 class WIFIReceiver():
@@ -10,14 +14,33 @@ class WIFIReceiver():
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((self.UDP_IP, self.UDP_port))
         self.on = False
+        self.phrases = ['','','','','']
 
     def start(self):
         self.on = True
         while self.on:
             data, addr = self.sock.recvfrom(1024)
             print('\nreceived following message\n--------------')
-            print(data)
+            print(json.loads(data.decode()))
+            for i, phraseobj in enumerate(json.loads(data.decode())):
+                self.phrases[i] = str(i+1) + '. ' +  phraseobj['speech']
             time.sleep(0.01)
 
 receiver = WIFIReceiver()
-receiver.start()
+t1 = threading.Thread(target=receiver.start).start()
+
+@app.route("/output")
+def output():
+    return render_template(
+            'index.html',
+            title='Conversation:',
+            phrase1=receiver.phrases[0],
+            phrase2=receiver.phrases[1],
+            phrase3=receiver.phrases[2],
+            phrase4=receiver.phrases[3],
+            phrase5=receiver.phrases[4]
+        )
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', '5010')
+
