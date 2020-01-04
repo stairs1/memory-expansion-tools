bbceac5893ffae5469dce1d21c57b2f1164c6656
jeremy <stairs1@github.com>
2020-01-04 12:13:11 -0500

    add missing form.py

---
+++ forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import InputRequired, ValidationError
+
+class SearchForm(FlaskForm):
+    search_item = StringField('Time', validators=[InputRequired()])
+    submit = SubmitField('Submit')
+
+    def validate_search_item(FlaskForm, field):
+        if len(field.data) > 30:
+            raise ValidationError('this field must be under 31 chars')
+
