c7733af719bcb1ee1cd46c4bd7ab2d0bd3c51211
Cayden Pierce <cpierc3@uwo.ca>
2020-01-05 23:00:51 -0500

    made everything fully REST

---
+++ Cache.py
@@ -0,0 +1,10 @@
+class Cache:
+    def __init__(self):
+        self.phrases_len = 10
+        self.stage_len = 8
+        self.phrases = []
+        self.stage = []
+        self.speech = ""
+        self.dataReady = False
+
+
--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 #flask
 from flask import Flask, render_template, flash, redirect, jsonify
-from flask_socketio import SocketIO, emit
 from flask import request
 from flask_restful import Api
 from bson.json_util import dumps
@@ -9,12 +8,17 @@ from bson.json_util import dumps
 import json
 from time import sleep
 import os
+import threading
 
 #custom stuff
 from db import Database
 from forms import SearchForm
 from api.SearchEndpoint import SearchPage
 from api.TimeFlowEndpoint import TimeFlow
+from api.MainEndpoint import Main
+from api.RememberEndpoint import Remember
+from api.PhraseSocket import PhraseSocket
+from Cache import Cache
 
 app = Flask(__name__)
 app.debug=True
@@ -23,58 +27,14 @@ app.config['SECRET_KEY'] = 'super special secret'
 
 api = Api(app) #flask_restful
 
-socketio = SocketIO(app)
-server_thread = None
-
-db = Database()
-
-phrases = []
-stage = []
-data_ready = False
-
-def send(_phrases, _stage):
-    global phrases, stage, data_ready
-    phrases = _phrases
-    stage = _stage
-    data_ready = True
-
-def remember(data): 
-    userId = data['userId']
-    reqType = data['type']
-    phraseList = data['phrases']
-
-    for phrase in phraseList:
-        timestamp = phrase['timestamp']
-        speech = phrase['speech']
-        if reqType == "talk":
-            resp = db.addTalk(userId, speech, timestamp)
-            print(resp)
-
-
-def server_poll():
-    global data_ready, db
-    while True:
-        sleep(0.1)
-        if data_ready:
-            socketio.emit('my_response', {'phrases': phrases, 'stage': stage})
-
-            data_ready = False
-
-@socketio.on('connect')
-def on_connect():
-    global server_thread
-    emit('my_response', {'phrases': ['nothing so far tbh'], 'stage': ['']})
-    if server_thread is None:
-        server_thread = socketio.start_background_task(server_poll)
-        
-@app.route('/')
-def main_page():
-    return render_template('convo.html')
-
 def start():
-    db.connect()
+    cache = Cache()
+    phraseSock = PhraseSocket(app, cache)
+    
+    api.add_resource(Main, '/')
     api.add_resource(SearchPage, '/search')
     api.add_resource(TimeFlow, '/timeflow')
+    api.add_resource(Remember, '/remember', resource_class_args=[app, cache])
     app.run()
 
 if __name__ == "__main__":
