adaa46f16b45649f724577dd03c93fa581a48801
stairs1 <jeremy.stairs@gmail.com>
2020-03-02 20:59:07 -0500

    vuzix ui

---
+++ vuzixmemory/app/src/main/java/com/example/vuzix_memory/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.vuzix_memory;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
+}
