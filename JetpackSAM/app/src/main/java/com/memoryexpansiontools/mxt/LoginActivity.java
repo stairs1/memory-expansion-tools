196404193bb7f7f51fd68817df3b69e08c709631
James Pierce <diegoatravesar@gmail.com>
2020-03-23 08:51:37 -0400

    Updated login logic
    * Login logic in LoginActivity now checks to make sure the user was successfully logged in before redirecting them to the main activity.
    * It notifies the user of the failed login by displaying the snackbar.

--- JetpackSAM/app/src/main/java/com/memoryexpansiontools/mxt/LoginActivity.java
+++ JetpackSAM/app/src/main/java/com/memoryexpansiontools/mxt/LoginActivity.java
@@ -2,11 +2,16 @@ package com.example.jetpacksam;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import org.json.JSONException;
 
 public class LoginActivity extends Activity {
     Button cancel, login;
@@ -34,13 +39,29 @@ public class LoginActivity extends Activity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //check if some creds are entered
-                if ((username.getText().toString() != null) && (password.getText().toString() != null)) {
+                //check if some creds are entered but not if they are valid
+                if ((username.getText().toString() != "") && (password.getText().toString() != "")) {
                     //login the user to the server
                     ServerAdapter server = new ServerAdapter(mContext);
                     server.login(username.getText().toString(), password.getText().toString());
-                    finish();
-                } else { //user hasn't entered correct credentials
+
+
+                    //check if the user was successfully logged in
+                    SharedPreferences sharedPref = server.lcontext.getSharedPreferences("mxt", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    String loggedIn = sharedPref.getString("token", "");
+                    String loggedInRefresh = sharedPref.getString("refreshtoken", "");
+                    CoordinatorLayout loginLayout = (CoordinatorLayout) findViewById(R.id.loginLayout);
+                    //if the user was logged in successfully, finish
+                    if(loggedIn != null){
+                        finish();
+                        //otherwise, display an alert indicating the login creds were invalid
+                    } else{
+                        Snackbar snackbar = Snackbar
+                                .make(loginLayout, "Your login credentials are invalid. Please try again.", Snackbar.LENGTH_LONG);
+                        snackbar.show();
+                    }
+                } else { //user hasn't entered anything as credentials
                 }
             }
         });
